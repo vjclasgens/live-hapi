a3f29f0e0e285175c686c7b1da438f2b070b8c78
V.J. Clasgens <vj@parkifi.com>
2015-02-08 09:04:39 -0700

    Initial commit for LiveH-API

---
+++ src/main/java/com/vanjenson/live/hapi/LiveHapiApplication.java
@@ -0,0 +1,12 @@
+package com.vanjenson.live.hapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveHapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveHapiApplication.class, args);
+    }
+}
